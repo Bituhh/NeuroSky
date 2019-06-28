3bdb4568f9a93d00ae6c47cf66c3c685dc9889a6
Bituhh <victor@oliveira.org.uk>
2019-06-28 20:08:02 +0100

    implementing RxPy for reactive programming, refactored to include Subject and simplifications.

--- _trainer.py
+++ _trainer.py
@@ -11,6 +11,7 @@ from numpy import array_equal, array
 from scipy.signal import savgol_filter, butter, lfilter
 from sklearn.decomposition import FastICA
 from sklearn.ensemble import RandomForestClassifier
+from rx.subjects import Subject
 
 
 class Trainer(object):
@@ -18,89 +19,104 @@ class Trainer(object):
         # Classifier Initializer
         self.cls = RandomForestClassifier(n_estimators=100)
 
-        # Training Parameters
-        self.is_trained = False
+        # disposal handler
+        self.subscriptions = []
 
-        self.prediction = 0
-        self.prediction_status = 'Initialising...'
-        self.training_counter = 0
+        # Observables and Subjects
+        self.prediction = Subject()
+        self.subscriptions.append(self.prediction)
+
+        # Training Params
+        self.training_wait_time = 3
+        self.recording_time = 3
+        self.is_recording_data = False
+        self.is_previously_trained = False
+        self.is_training = False
+        self.current_training_target = None
         self.sample = []
         self.target = []
-        self.confidence_level = []
+        # self.training_counter = 0
+
+        # Prediction Params
+        self.prediction_wait_time = 0.25
+        self.current_data = []
+
+        # Prediction Initializer
+        # self.confidence_level = []
         self._init_thread(target=self.predict)
+
+        '''
+        Handling data steps:
+        wait a time interval
+        record data for a set time interval
+        score data if previously trained
+        train on data set.
+        '''
 
     @staticmethod
     def _init_thread(target, args=()):
         threading.Thread(target=target, args=args).start()
 
-    def train(self, target):
-        self._init_thread(target=self._train_thread, args=(target,))
+    def add_data(self, data):
+        self.current_data = data
+        if self.is_recording_data:
+            self.sample.append(self.current_data)
+            self.target.append(self.current_training_target)
 
-    def _train_thread(self, target):
-        # self.cls.n_estimators += 1
-        # try:
-        self.prediction_status = 'Waiting 3 sec...'
-        sleep(3)
-        self.prediction_status = 'Recording Data...'
-        recorded_data = []
-        recorded_target = []
-        start_time = time()
-        previous = array([])
-        while (time() - start_time) < 3:
-            current = self.fft_data[1]
-            if not array_equal(current, previous):
-                recorded_data.append(current)
-                recorded_target.append(int(target))
-                previous = current
-        self.prediction_status = 'Storing Data...'
-        for i in range(len(recorded_data)):
-            self.sample.append(recorded_data[i])
-            self.target.append(recorded_target[i])
-        has_confidence_level = False
-        if self.is_trained:
-            self.prediction_status = 'Scoring Data...'
-            self.confidence_level.append(
-                [self.cls.score(recorded_data, recorded_target), self.training_counter, int(target)])
-            print(self.confidence_level[-1])
-            has_confidence_level = True
-        self.is_trained = False
-        self.prediction_status = 'Training...'
+    def train(self, target):
+        self.is_training = True
+        self.current_training_target = target
+        sleep(self.training_wait_time)
+        self.is_recording_data = True
+        sleep(self.recording_time)
+        self.is_recording_data = False
+        # score previous training
+        # if self.is_previously_trained:
+        #     self.confidence_level.append(
+        #         [self.cls.score(recorded_data, recorded_target), self.training_counter, int(target)])
+        #     print(self.confidence_level[-1])
+        #     has_confidence_level = True
         self.cls.n_estimators += 1
         training_start_time = time()
         self.cls.fit(self.sample, self.target)
-        if has_confidence_level:
-            self.confidence_level[-1].append(time() - training_start_time)
-            print(self.confidence_level[-1][2])
-        print('done fitting')
-        self.is_trained = True
-        self.prediction_status = 'Ready...'
-        self.training_counter += 1
-        # except:
-        #     print('Error on training!')
-        #     pass
-    #
-    # def predict(self):
-    #     while not self.is_closed:
-    #         try:
-    #             if self.is_trained and (
-    #                     self.prediction_status not in ['Waiting 3 sec...', 'Recording Data...', 'Storing Data...',
-    #                                                    'Scoring Data...']):
-    #                 self.prediction_status = 'Prediction...'
-    #                 # print(len(self.fft_data[1]), '< y .... x >', len(self.fft_data[0]))
-    #                 if self.cls.predict([self.fft_data[1]])[0] == 0:
-    #                     self.prediction = 'Arm Up'
-    #                 else:
-    #                     self.prediction = 'Arm Down'
-    #                 sleep(0.25)
-    #         except:
-    #             pass
-    #             print('Error on predicting!')
+        print(training_start_time)
+        self.is_training = False
+        # if has_confidence_level:
+        #     self.confidence_level[-1].append(time() - training_start_time)
+        #     print(self.confidence_level[-1][2])
+        # print('done fitting')
+        # self.is_trained = True
+        # self.prediction_status = 'Ready...'
+        # self.training_counter += 1
+
+    def predict(self):
+        if self.is_previously_trained and not self.is_training:
+            try:
+                if self.cls.predict(self.current_data)[0] == 0:
+                    self.prediction = 'Arms Up'
+                else:
+                    self.prediction = 'Arms Down'
+                sleep(self.prediction_wait_time)
+            except:
+                print('An error occurred on prediction, prediction not performed!')
+
+    def close(self):
+        for subscription in self.subscriptions:
+            subscription.dispose()
 
 
 if __name__ == '__main__':
     connector = Connector(debug=True, verbose=False)
     processor = Processor()
-    processor.fft_data.subscribe(lambda value: print(value))
+    trainer = Trainer()
 
-    while connector.is_open:
-        processor.append_data(connector.data)
+    connector.data.subscribe(processor.add_data)
+    processor.data.subscribe(trainer.add_data)
+    counter = 0
+    while counter < 1000:
+        counter += 1
+    trainer.train(0)
+    sleep(60)
+    connector.close()
+    processor.close()
+    trainer.close()
